31a15f43fea3f1e83bed850d65b304c7be55c9af
Sidsel Boldsen <sludsle@gmail.com>
2021-02-19 00:58:39 +0100

    Add solution to second exercise

--- day2/buggy/dicegame/runner.py
+++ day2/buggy/dicegame/runner.py
@@ -3,28 +3,26 @@ from .utils import i_just_throw_an_exception
 
 class GameRunner:
 
-    def __init__(self):
+    def __init__(self, n_round=1, wins=0, loses=0):
         self.dice = Die.create_dice(5)
-        self.reset()
-
-    def reset(self):
-        self.round = 1
-        self.wins = 0
-        self.loses = 0
+        self.round = n_round
+        self.wins = wins
+        self.loses = loses
 
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value
         return total
 
     @classmethod
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
+        c = 0   # Consecutive wins
+        runner = cls()
+
         while True:
-            runner = cls()
 
             print("Round {}\n".format(runner.round))
 
@@ -44,7 +42,7 @@ class GameRunner:
                 print("Like seriously, how could you mess that up")
                 runner.loses += 1
                 c = 0
-            print("Wins: {} Loses {}".format(runner.wins, runner.loses))
+            print("Wins(consecutive): {} ({}) Loses {}".format(runner.wins, c, runner.loses))
             runner.round += 1
 
             if c == 6:
@@ -54,7 +52,8 @@ class GameRunner:
 
             prompt = input("Would you like to play again?[Y/n]: ")
 
-            if prompt == 'y' or prompt == '':
+            if prompt.lower() == 'y' or prompt == '':
+                runner = cls(runner.round, runner.wins, runner.loses)
                 continue
             else:
                 i_just_throw_an_exception()
--- day2/buggy/main.py
+++ day2/buggy/main.py
@@ -1,12 +1,19 @@
+import sys
 from dicegame.runner import GameRunner
 
 
-def main():
+def main(mode="run"):
     print("Add the values of the dice")
     print("It's really that easy")
     print("What are you doing with your life.")
+    if mode=="debug":
+        import ipdb; ipdb.set_trace()
     GameRunner.run()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        mode = sys.argv[1]
+        main(mode)
+    except IndexError:
+        main()
